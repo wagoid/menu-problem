eb35308a4acf55728311a4379b56e71fbc7d8964
wagoid <w.rsantos@outlook.com>
2016-04-18 19:25:22 -0300

    File read logic is now made line by line.

--- src/menuSolver.js
+++ src/menuSolver.js
@@ -1,65 +1,80 @@
 var fs = require('fs');
 var path = require('path');
+var readLine = require('readline');
 
 solveMenuProblem();
 
 function solveMenuProblem() {
-  var problemInfo = getProblemFromFile(path.join(__dirname, 'problems.txt'));
-  
-  
+  getProblemFromFileAsync(path.join(__dirname, 'problems.txt'))
+    .then(problemInfo => {
+      console.log(JSON.stringify(problemInfo, null, 4));
+    })
+    .catch(e => {
+      console.log(e);
+    });
 }
 
-function getNextProblemItems(problemArray, initialIndex, jumps) {
-  var result = [];
-  
-  for (var i = initialIndex; i < (initialIndex + jumps) && i < problemArray.length; i++) {
-    var splitted = problemArray[i].split(' ');
-    result.push({ cost: +splitted[0], value: +splitted[1] });
-  }
-  
-  return result;
-}
-
-function getProblemFromFile(fileName) {
-  if (fs.existsSync(fileName)) {
-    var data = fs.readFileSync(fileName, 'utf8');
-    data = data.replace(/\r/g, '');
-    var problemArray = data.split('\n');
+function getProblemFromFileAsync(fileName) {
+  return new Promise((resolve, reject) => {
+    if (!fs.existsSync(fileName)) {
+      return reject(`The file ${fileName} was not found.`);
+    }
     var problemInfo = [];
+    var lineReader = readLine.createInterface({
+      input: fs.createReadStream(fileName)
+    });
     
-    for (var i = 0; i < problemArray.length; i++) {
-      var splitted = problemArray[i].split(' ');
+    var platesNumber = 0;
+    var readPlates = 0;
+      
+    lineReader.on('line', (line) => {
+      var splitted = line.split(' ');
       
       if (splitted.length === 3) {
+        readPlates = 0;
         var days = +splitted[0];
-        var platesNumber = +splitted[1];
+        platesNumber = +splitted[1];
         var budget = +splitted[2];
         
-        if (days !== 0 && platesNumber !== 0 && budget !== 0) {
-          var plates = getNextProblemItems(problemArray, i + 1, platesNumber);
+        if (isNaN(days) || isNaN(platesNumber) || isNaN(budget)) {
+          return reject(`Unrecognized line format (${line}).`);
+        }
+        
+        if (days === 0 && platesNumber === 0 && budget === 0) {
+          lineReader.close();
+        } else {
           problemInfo.push({
             days,
             platesNumber,
             budget,
-            plates
+            plates: []
           });
-          
-          i = i + platesNumber;
-          if (i === problemArray.length - 1) {
-            break;
-          }
         }
+          
       } else if (splitted.length === 2) {
+        readPlates++;
+        var lastProblem = problemInfo[problemInfo.length -1];
+        var plateCost = +splitted[0];
+        var plateValue = +splitted[1];
         
+        if (isNaN(plateCost) || isNaN(plateValue)) {
+          return reject(`Unrecognized line format (${line}).`);
+        }
+        if (readPlates > platesNumber) { 
+          return reject(`There were more plates than the specified in the previous line (${lastProblem.days} ${lastProblem.platesNumber} ${lastProblem.budget}).`);
+        }
+        
+        var plate = { cost: +splitted[0], value: +splitted[1] };
+        lastProblem.plates.push(plate);
       } else {
-        
+        return reject(`Unrecognized line format (${line}).`);
       }
-    }
-    
-    return problemInfo;
-  } else {
-    console.log("The problems.txt file does not exist.");
-  }
+    });
+
+    lineReader.on('close', () => {
+      resolve(problemInfo);
+    });
+  });
 }
 
 module.exports = solveMenuProblem;
